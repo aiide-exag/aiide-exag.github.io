9f8c15ac9e38e4e2049835965f070203ce77210d
Johor Jara <13amsi@gmail.com>
2024-06-03 13:18:52 -0600

    Update committee.tsx
    program committee TBD and workshop oirganizers update

--- src/pages/committee.tsx
+++ src/pages/committee.tsx
@@ -5,132 +5,29 @@ import Seo from '../components/Seo';
 
 const ORGANIZING_COMMITTEE = [
     {
-        name: 'Abdelrahman Madkour',
-        email: 'madkour.a@husky.neu.edu',
-        affiliation: 'Northeastern University',
-    },
-    {
-        name: 'Jasmine Otto',
-        email: 'jtotto@ucsc.edu',
-        affiliation: 'University of California, Santa Cruz',
-    },
-    {
-        name: 'Lucas N. Ferreira',
-        email: 'lnferrei@ualberta.ca',
+        name: 'Emily Halina',
+        email: 'ehalina@ualberta.ca',
         affiliation: 'University of Alberta',
     },
     {
-        name: 'Shi Johnson-Bey',
-        email: 'ismajohn@ucsc.edu',
-        affiliation: 'University of California, Santa Cruz',
+        name: 'Johor Jara Gonzalez',
+        email: 'jaragonz@ualberta.ca',
+        affiliation: 'University of Alberta',
     },
+    {
+        name: 'Mira Fisher',
+        email: 'mira.fisher@uky.edu',
+        affiliation: 'University of Kentuchy',
+    },
+    
 ];
 
 const PROGRAM_COMMITTEE: { name: String; affiliation: String }[] = [
     {
-        name: 'Matthew Barthet',
-        affiliation: 'Institute of Digital Games, University of Malta',
+        name: 'TBD',
+        affiliation: 'TBD',
     },
-    {
-        name: 'Morteza Behrooz',
-        affiliation: 'Worcester Polytechnic Institute',
-    },
-    {
-        name: 'Colon Biemer',
-        affiliation: 'Northeastern University',
-    },
-    {
-        name: 'Vadim Bulitko',
-        affiliation: 'University of Alberta',
-    },
-    {
-        name: 'Sara Cardinale',
-        affiliation: 'Queen Mary University of London',
-    },
-    {
-        name: 'M Charity',
-        affiliation: 'New York University ',
-    },
-    {
-        name: 'Seth Cooper',
-        affiliation: 'Northeastern University',
-    },
-    {
-        name: 'Sam Earle',
-        affiliation: 'New York University',
-    },
-    {
-        name: 'Sean Guillory',
-        affiliation: 'Booz Allen Hamilton',
-    },
-    {
-        name: 'Matthew Guzdial',
-        affiliation: 'University of Alberta',
-    },
-    {
-        name: 'Emily Halina',
-        affiliation: 'University of Alberta',
-    },
-    {
-        name: 'Ian Horswill, Associate Professor',
-        affiliation: 'Northwestern University',
-    },
-    {
-        name: 'Ahmed Khalifa',
-        affiliation: 'University of Malta',
-    },
-    {
-        name: 'Max Kreminski',
-        affiliation: 'Santa Clara University',
-    },
-    {
-        name: 'Sylvain Lapeyrade',
-        affiliation: 'Paul Sabatier University',
-    },
-    {
-        name: 'Dr. Peter Mawhorter',
-        affiliation: 'Wellesley College',
-    },
-    {
-        name: 'Allison Parrish, Assistant Arts Professor',
-        affiliation: 'New York University',
-    },
-    {
-        name: 'Nathan Partlan',
-        affiliation: 'Northeastern University',
-    },
-    {
-        name: 'Johannes Pfau',
-        affiliation: 'University of California Santa Cruz',
-    },
-    {
-        name: 'Ben Samuel',
-        affiliation: 'University of New Orleans',
-    },
-    {
-        name: 'Akash Saravanan',
-        affiliation: 'University of Alberta',
-    },
-    {
-        name: 'Laurissa Tokarchuk',
-        affiliation: 'Queen Mary University of London',
-    },
-    {
-        name: 'Jim Whitehead',
-        affiliation: 'University of California Santa Cruz',
-    },
-    {
-        name: 'Oliver Withington',
-        affiliation: 'Queen Mary University of London',
-    },
-    {
-        name: 'Richard Zhao',
-        affiliation: 'University of Calgary',
-    },
-    {
-        name: 'Dr. Alex Zook',
-        affiliation: 'Blizzard Entertainment',
-    },
+    
 ];
 
 const CommitteePage = () => {
