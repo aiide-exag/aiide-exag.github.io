ac5ca00db11be94d59c514e3da2aa467875556f3
Kaylah Facey <kaylah.facey@gmail.com>
2025-09-04 18:57:49 -0400

    add review deadline

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,35 +36,16 @@ const IndexPage = () => {
                     </p>
                     <h2 className="mb-2">Announcements</h2>
                     <div className="announcement">
-                        <h3>Deadline Extended!</h3>
+                        <h3>Submissions Under Review!</h3>
                         <p>
-                            Submissions are now due on <b>August 29th, 2025</b><br />
-                            <a
-                                href="/call_for_papers"
-                                target="_blank"
-                                rel="noopener"
-                            >
-                                Call for Papers
-                            </a>
+                            Thank you all for the unprecedented number of submissions!
+                            Reviews are due on <b>September 18th, 2025</b><br />
                         </p>
                     </div>
                     <div className="announcement">
-                        <h3>2025 workshop dates!</h3>
+                        <h3>2025 Workshop Dates!</h3>
                         <p>
                             The workshop will be held on November 10th & 11th.
-                        </p>
-                    </div>
-                    <div className="announcement">
-                        <h3>2025 Call for Papers</h3>
-                        <p>
-                            Submissions are due on <b>August 29th, 2025</b><br />
-                            <a
-                                href="/call_for_papers"
-                                target="_blank"
-                                rel="noopener"
-                            >
-                                Call for Papers
-                            </a>
                         </p>
                     </div>
                     <h2 className="mb-2">Registration</h2>
